43fc4790cc5f5fa5927840a021134322f5249869
cheesekon <cheesekon@163.com>
2022-03-16 13:11:27 +0800

    vitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './pages/App'
-import reportWebVitals from './reportWebVitals'
+// import reportWebVitals from './reportWebVitals'
 import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
 import { HashRouter } from 'react-router-dom'
